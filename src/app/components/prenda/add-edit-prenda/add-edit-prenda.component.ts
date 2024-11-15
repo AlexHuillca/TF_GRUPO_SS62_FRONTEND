2c6a7a42967c302c0e8d948f85e5aed570e427c2
tukibuki <littletinkle1507@gmail.com>
2024-11-15 14:20:32 -0500

    xiomara-v5
    xiomara-v5

--- src/app/components/prenda/add-edit-prenda/add-edit-prenda.component.ts
+++ src/app/components/prenda/add-edit-prenda/add-edit-prenda.component.ts
@@ -20,6 +20,8 @@ export class AddEditPrendaComponent {
   prendaId: number = 0;
   marcas: Marca[] = [];
   categorias: CategoriaPrenda[] = [];
+  tallas: string[] = ['XS', 'S', 'M', 'L', 'XL']; 
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -55,15 +57,7 @@ export class AddEditPrendaComponent {
       // Cuando queremos actualizar
       this.prendaService.getPrendaById(this.prendaId).subscribe({
         next: (data: Prenda) => {
-          this.addEditForm.get("id")?.setValue(data.id);
-          this.addEditForm.get("nombrePrenda")?.setValue(data.nombrePrenda);
-          this.addEditForm.get("tipoPrenda")?.setValue(data.tipoPrenda);
-          this.addEditForm.get("colorPrenda")?.setValue(data.colorPrenda);
-          this.addEditForm.get("talla")?.setValue(data.talla);
-          this.addEditForm.get("precioOriginal")?.setValue(data.precioOriginal);
-          this.addEditForm.get("precioFinal")?.setValue(data.precioFinal);
-          this.addEditForm.get("idMarca")?.setValue(data.idMarca.idMarca);
-          this.addEditForm.get("idCategoria")?.setValue(data.idCategoria.idCategoria);
+          this.addEditForm.patchValue(data);
         },
         error: (err) => {
           console.log(err);
@@ -114,6 +108,8 @@ export class AddEditPrendaComponent {
         next: (data) => {
           this.router.navigate(["/prendas"]);
           this.snackbar.open("La prenda fue registrada correctamente", "OK", { duration: 2000 });
+          this.cargarMarcas(); // Actualiza las marcas
+          this.cargarCategorias(); // Actualiza las categorías
         },
         error: (err) => {
           console.log(err);
@@ -125,6 +121,8 @@ export class AddEditPrendaComponent {
         next: (data) => {
           this.router.navigate(["/prendas"]);
           this.snackbar.open("La prenda fue actualizada correctamente", "OK", { duration: 2000 });
+          this.cargarMarcas(); // Actualiza las marcas
+          this.cargarCategorias(); // Actualiza las categorías
         },
         error: (err) => {
           console.log(err);
